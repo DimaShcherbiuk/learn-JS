59b3c5ec3771118fc7bd3cc66d528f2d2ecd82f6
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-01-19 20:39:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-'use strict';
-
-console.log("JavaScript is awesome!");
-console.log(10);
+const start = 11;
+const end = 15;
+let number;
+for (let i = start; i < end; i++) {
+  if(i % 5 === 0){
+      number = i;
+      console.log(number);
+    break;
+  }
+}
