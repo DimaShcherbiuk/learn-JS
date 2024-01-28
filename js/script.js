3208c277e73aac0e5ccb9e1c0cc9a2fc02cd41ba
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-01-28 13:49:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
-function createReversedArray() {
-
-    const argsArray = Array.from(arguments);
-    
-    const reversedArray = argsArray.toReversed();
-    
-    return reversedArray;
-}
-
-// Приклади викликів функції
-console.log(createReversedArray(12, 85, 37, 4));        // [4, 37, 85, 12]
-console.log(createReversedArray(164, 48, 291));          // [291, 48, 164]
-console.log(createReversedArray(412, 371, 94, 63, 176)); // [176, 63, 94, 371, 412]
-console.log(createReversedArray());   
+const a = 0;
+console.log(typeof a)
