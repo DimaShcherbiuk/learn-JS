2496d94cb855db6cc32ca34c43fb7b2eaf8b3ed2
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-01-29 22:41:36 +0200

    add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-const a = 0;
-console.log(typeof a)
+function getExtremeScores(scores) {
+
+  // let max = Math.max(...scores);
+  // let min = Math.min(...scores);
+  
+  let objectMinMax = {
+    best: Math.max(...scores),
+    wrost: Math.min(...scores),
+  };
+  return objectMinMax;
+}
+
+
+console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
+console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
