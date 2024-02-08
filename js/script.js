e89bf636032881044824b6e1fba8f8642213bf2c
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-02-08 21:48:22 +0200

    іфвів

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-function getExtremeScores(scores) {
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
 
-  // let max = Math.max(...scores);
-  // let min = Math.min(...scores);
+const names = books
   
-  let objectMinMax = {
-    best: Math.max(...scores),
-    wrost: Math.min(...scores),
-  };
-  return objectMinMax;
-}
+  .filter((book) => book.rating > MIN_BOOK_RATING)
+  .map(book => book.author)
+  .toSorted((a, b) => a.localeCompare(b));
 
-
-console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
-console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+console.log(names)
+  
+  // .toSorted((a, b) => a.author.localeCompare(b.author));
