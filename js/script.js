534d512c84a0e22495a63aab13615a2f4f0d8003
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-03-04 21:18:12 +0200

    safsd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,71 @@
-console.log(_.sum([4, 2, 8, 6])); // 20
-console.log(_.sum([5, 10])); // 15
+// const settings = {
+//   theme: "dark",
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
 
-console.log(_.shuffle([1, 2, 3, 4])); // [4, 1, 3, 2]
-console.log(_.shuffle([1, 2, 3, 4])); // [3, 2, 1, 4]
+// localStorage.setItem("settings", JSON.stringify(settings));
+
+// localStorage.setItem("ui-theme", "light");
+
+// const savedTheme = localStorage.getItem("ui-theme");
+// console.log(savedTheme); // "light"
+
+// const savedItem = localStorage.getItem("key-that-does-not-exist");
+// console.log(savedItem); // null
+
+// const settings = {
+//   theme: "dark",
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
+// localStorage.setItem("settings", JSON.stringify(settings));
+
+// const savedSettings = localStorage.getItem("settings");
+// console.log(savedSettings); // A string
+
+// const parsedSettings = JSON.parse(savedSettings);
+// console.log(parsedSettings); // Settings object
+
+// localStorage.setItem("ui-theme", "dark");
+// console.log(localStorage.getItem("ui-theme")); // "dark"
+
+// localStorage.removeItem("ui-theme");
+// console.log(localStorage.getItem("ui-theme")); // null
+// console.log(window.sessionStorage); // Storage {length: 0}
+
+// sessionStorage.setItem("user-id", "123");
+// sessionStorage.setItem(
+//   "tickets",
+//   JSON.stringify({ from: "Lviv", to: "Kyiv", quantity: 2 })
+// );
+// console.log(sessionStorage);
+// // Storage {user-id: '123', tickets: '{"from":"Lviv","to":"Kyiv","quantity":2}', length: 2}
+// const userId = sessionStorage.getItem("user-id");
+// console.log(userId); // "123"
+
+// const tickets = JSON.parse(sessionStorage.getItem("tickets"));
+// console.log(tickets); // { from: "Lviv", to: "Kyiv", quantity: 2 }
+
+// sessionStorage.removeItem("tickets");
+// console.log(sessionStorage); // Storage {user-id: '123', length: 1}
+
+// sessionStorage.clear();
+// console.log(sessionStorage); // Storage {length: 0}
+
+const form = document.querySelector(".feedback-form");
+const textarea = form.elements.message;
+const localStorageKey = "goit-example-message";
+
+textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  console.log(evt.target.elements.message.value);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
